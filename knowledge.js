a3f203543796498e8ed1e319c81c6cdf98c9f830
Alex Eales <alexeales@hotmail.co.uk>
2019-04-02 11:59:37 +0100

    Knowledge Graph data structure creation + Created the graph structure + Created template method - In process of moving over sql to GCP so can start serving actual queries.

--- knowledge.js
+++ knowledge.js
@@ -1,5 +1,14 @@
 const datastore = require('./datastore');
 const DEFAULT_RESPONSE = `Sorry, I couldn't find anything to help with that!`;
+const graph = {
+    ['geo-city']: {
+        ServiceProvider: servicesNearLoc
+    }
+};
+
+function servicesNearLoc(params) {
+    return `Returning services near: ${params['geo-city']}`;
+}
 
 // Example:
 // let trusts = [];
@@ -11,5 +20,12 @@ const DEFAULT_RESPONSE = `Sorry, I couldn't find anything to help with that!`;
 
 exports.query = function (intent, entities) {
     console.log(`Querying the graph for mapping: ${Object.keys(entities)} -> ${intent}`);
+    const inputs = Object.keys(entities);
+    if (inputs in graph) {
+        if (intent in graph[inputs]) {
+            console.log(`Found transform: ${graph[inputs][intent]}`);
+            return graph[inputs][intent](entities);
+        }
+    }
     return DEFAULT_RESPONSE;
 };
