931fe3a66c93c54d1c2b0c1895864193a643dcc3
Alex Eales <alexeales@hotmail.co.uk>
2019-04-02 09:59:07 +0100

    Added Debugging message for Heroku Database Connection Testing

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const datastore = require('./datastore');
 const express = require('express');
 const app = express();
 
+console.log(`Env variables: Host = ${process.env.DB_HOST} Db = ${process.env.DB_NAME} User = ${process.env.DB_USER} Pass = ${process.env.DB_PASS}`)
+
 let trusts = [];
 datastore.datastore().Trusts.findAll({raw: true, attributes: ['Id', 'Code', 'Name']}).then(values => {
     trusts = values;
