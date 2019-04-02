9f73a69384869662e4f0bffc878d566cac10da6f
Alex Eales <alexeales@hotmail.co.uk>
2019-04-02 10:43:49 +0100

    Testing DialogFlow Fulfillment integration + Disabled sequelize for testing since cannot get through bath firewall.

--- index.js
+++ index.js
@@ -1,14 +1,22 @@
-const datastore = require('./datastore');
+// const datastore = require('./datastore');
 const express = require('express');
 const app = express();
+app.use(express.json());
 
 console.log(`Env variables: Host = ${process.env.DB_HOST} Db = ${process.env.DB_NAME} User = ${process.env.DB_USER} Pass = ${process.env.DB_PASS}`)
 
-let trusts = [];
-datastore.datastore().Trusts.findAll({raw: true, attributes: ['Id', 'Code', 'Name']}).then(values => {
-    trusts = values;
-});
+// let trusts = [];
+// datastore.datastore().Trusts.findAll({raw: true, attributes: ['Id', 'Code', 'Name']}).then(values => {
+//     trusts = values;
+// });
+//
+// app.get('/trusts', (req, res) => res.send(trusts));
 
-app.get('/trusts', (req, res) => res.send(trusts));
+app.post('/dialogflow', function (req, res) {
+    console.log(req.body);
+    res.send({
+        fulfillmentText: 'Hi from the API!'
+    });
+});
 
 app.listen(3000, () => console.log('Example app listening on port 3000.'));
