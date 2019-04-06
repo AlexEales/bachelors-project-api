7c633c9c8a1ae2aad3ba1add136ad218fdfb4c89
Alex Eales <alexeales@hotmail.co.uk>
2019-04-06 10:55:15 +0100

    Added in proper way to enable CORS - I might be an idiot that can't read documentation (will be proven in testing)

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ require('dotenv').config();
 
 const app = express();
 app.use(express.json());
-app.use(cors);
+app.use(cors());
 
 const projectId = process.env.DIALOGFLOW_ID;
 const sessionId = uuid.v4();
