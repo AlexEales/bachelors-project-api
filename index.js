0eed205e28ae1b6b3d3905e6ef8a70aa1c98524c
Alex Eales <alexeales@hotmail.co.uk>
2019-04-09 11:24:30 +0100

    Added in Checks for Knowledgebase answers by using v2beta1 version.

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const dialogflow = require('dialogflow');
+const dialogflow = require('dialogflow').v2beta1;
 const datastore = require('./datastore');
 const knowledge = require('./knowledge');
 const express = require('express');
@@ -16,6 +16,8 @@ const sessionClient = new dialogflow.SessionsClient({
     credentials: JSON.parse(process.env.DIALOGFLOW_KEY)
 });
 const sessionPath = sessionClient.sessionPath(projectId, sessionId);
+const knowbase = new dialogflow.KnowledgeBasesClient();
+const knowbasePath = knowbase.knowledgeBasePath(projectId, 'MTU1MjQ2MjAzOTkwODA4OTg1NjA');
 
 console.log(`Env variables: Port = ${process.env.PORT} Host = ${process.env.DB_HOST} Db = ${process.env.DB_NAME} 
     User = ${process.env.DB_USER} Pass = ${process.env.DB_PASS}`);
@@ -27,11 +29,12 @@ app.get('/trusts', function (req, res) {
 });
 
 app.post('/dialogflow', async function (req, res) {
+    console.log('Dialogflow:');
     console.log(req.body);
     const intent = req.body.queryResult.intent.displayName;
     const entities = req.body.queryResult.parameters;
     console.log(req.body.queryResult.fulfillmentText);
-    if (req.body.queryResult.fulfillmentText !== '') {
+    if (req.body.queryResult.fulfillmentText !== undefined) {
         res.send({
             fulfillmentText: req.body.queryResult.fulfillmentText
         });
@@ -43,6 +46,7 @@ app.post('/dialogflow', async function (req, res) {
 });
 
 app.post('/message', async function (req, res) {
+    console.log('Message:');
     console.log(req.body);
     const message = req.body.text;
     const request = {
@@ -52,14 +56,24 @@ app.post('/message', async function (req, res) {
                 text: message,
                 languageCode: 'en'
             }
+        },
+        queryParams: {
+            knowledgeBaseNames: [knowbasePath]
         }
     };
     const responses = await sessionClient.detectIntent(request);
+    console.log('Responses:');
     console.log(responses);
     const result = responses[0].queryResult;
-    res.send({
-        text: result.fulfillmentMessages[0].text.text[0]
-    });
+    if (result.knowledgeAnswers !== null && result.knowledgeAnswers.answers) {
+        res.send({
+            text: result.knowledgeAnswers.answers[0].answer
+        });
+    } else {
+        res.send({
+            text: result.fulfillmentMessages[0].text.text[0]
+        });
+    }
 });
 
 app.listen(process.env.PORT, () => console.log('Example app listening on port 3000.'));
